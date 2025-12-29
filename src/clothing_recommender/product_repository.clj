3de56755b8c7a23e75901250b5ebdb4d7f52fed0
sara-slavkovic <saraslavkovic3@gmail.com>
2025-12-30 00:55:23 +0100

    Normalize products from database

--- src/clothing_recommender/product_repository.clj
+++ src/clothing_recommender/product_repository.clj
@@ -2,10 +2,21 @@
   (:require [next.jdbc :as jdbc]
             [clothing-recommender.db :as db]))
 
+(defn normalize-product
+  [p]
+  {:product-id   (:PRODUCTS/PRODUCT_ID p)
+   :product-name (:PRODUCTS/PRODUCT_NAME p)
+   :brand        (:PRODUCTS/BRAND p)
+   :category     (:PRODUCTS/CATEGORY p)
+   :price        (:PRODUCTS/PRICE p)
+   :rating       (:PRODUCTS/RATING p)
+   :color        (:PRODUCTS/COLOR p)
+   :size         (:PRODUCTS/SIZE p)})
+
 (defn find-all
   []
-  (jdbc/execute! db/ds
-                 ["SELECT * FROM products"]))
+  (->> (jdbc/execute! db/ds ["SELECT * FROM products"])
+       (map normalize-product)))
 
 (defn find-by-category
   [category]
